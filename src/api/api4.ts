7542a56941dfa78cba4f87173314bf148136c202
Vlados <vlados@MacBook-Pro-Vlados.local>
2023-01-26 18:56:28 +0200

    txt

--- src/api/api4.ts
+++ src/api/api4.ts
@@ -1,12 +1,4 @@
 import axios from "axios";
 
 export const $api2 = axios.create({baseURL: '/api/',});
-
-import axios from "axios";
-
-export const $api2 = axios.create({baseURL: '/api/',});
-
-import axios from "axios";
-
-export const $api2 = axios.create({baseURL: '/api/',});
-
+/////i
